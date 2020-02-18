971071f262ef739bed7d55d6face2e2765a812d6
Thomas Tracy <contact@tjtracy.com>
2020-02-18 14:10:31 -0500

    Add phone_number field to UserProfile model
    Adds a simple nullable field to the UserProfile model. The only
    validation done it to make sure any character saved in that field is a
    digit and not a letter. We do not distiguish on a model level if the
    phone number is international or not.

--- common/djangoapps/student/models.py
+++ common/djangoapps/student/models.py
@@ -18,7 +18,7 @@ import logging
 import uuid
 from collections import OrderedDict, defaultdict, namedtuple
 from datetime import datetime, timedelta
-from django.core.validators import FileExtensionValidator
+from django.core.validators import FileExtensionValidator, RegexValidator
 from functools import total_ordering
 from importlib import import_module
 
@@ -505,7 +505,8 @@ class UserProfile(models.Model):
     allow_certificate = models.BooleanField(default=1)
     bio = models.CharField(blank=True, null=True, max_length=3000, db_index=False)
     profile_image_uploaded_at = models.DateTimeField(null=True, blank=True)
-
+    phone_regex = RegexValidator(regex=r'^\+?1?\d*$', message="Phone number can only contain numbers.")
+    phone_number = models.CharField(validators=[phone_regex], blank=True, null=True, max_length=50)
     @property
     def has_profile_image(self):
         """
